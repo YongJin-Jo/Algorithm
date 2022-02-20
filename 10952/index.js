7d4aa6b2cc590fb20aa003c3c6be3ec361465c4f
YongJin-Jo <artinc5233@gmail.com>
2022-02-20 19:44:55 +0900

    while 문제 정리

---
+++ 10952/index.js
@@ -0,0 +1,10 @@
+const fs = '1 1\n2 3\n3 4\n9 8\n5 2\n0 0'
+const input = fs.toString().trim().split('\n').map(item => item.split(' '));
+let i =0;
+let answer = ''
+while(i < input.length -1){
+  target= input[i]
+  answer = answer + ((+target[0])+(+target[1])+'\n')
+  i++
+}
+console.log(answer);
---
+++ 1110/index.js
@@ -0,0 +1,19 @@
+//1의 자리에 있는 수 구하기: num % 10
+//10의 자리에 있는 수 구하기: Math.floor(num / 10)
+//1의 자리에 있는 수 10의 자리 수로 만들기 : (num % 10) * 10
+
+const fs = '26';
+let num = +fs.toString().trim();
+let i =0;
+
+while(true){
+  i++
+  sum = Math.floor(num/10) + num % 10; //8
+
+  num = (num %10) *10 + sum %10; //68
+
+  if (input === num) {
+    break;
+  } 
+}
+console.log( i);
