f6ef5e06e1b20145225473b93a6af0813e0ce836
YongJin-Jo <artinc5233@gmail.com>
2022-03-08 22:57:26 +0900

    백준 기본 수학 2 문제 풀이: 골드바흐의 추측

--- 백준/단계별 풀이/기본수학2/9020.js
+++ 백준/단계별 풀이/기본수학2/9020.js
@@ -1,32 +1,27 @@
 const fs = '3\n8\n10\n16'
 const input = fs.toString().split('\n').map(item => +item)
+input.shift();
+const MAX = Math.max(...input);
+let prime = new Array(MAX + 1).fill(true);
+let answer = [];
+prime[0] = prime[1] = false;
 
-solution(input)
-
-function solution(arg){
-  for(let i =0; i< arg; i++){
-    const N = arg[i]
-    const isPrime = Array(N+1).fill(true)
-    let primeArr =[]
-    isPrime[0] = isPrime[1] = false
-
-    for(let i =2; i<= Math.ceil(Math.sqrt(N)); i++ ){
-      if(isPrime[i]){
-        let m = 2
-        while(i*m <N){
-          isPrime[i*m] = false
-          m++
-        }
-      }
+for(let i =2; i<=MAX+1;i++){
+  if(prime[i]){
+    let m = 2
+    while(i*m < MAX+1){
+      prime[i*m] = false
+      m++
     }
-
-    for(let i=2; i<=N; i++){
-      if(isPrime[i]){
-        primeArr.push(i)
-      }
-    }
-    
-    console.log();
   }
 }
 
+input.forEach(v => {
+  for(let i=Math.ceil(v/2); i>1; i--){
+    if(prime[i] && prime[v-i]){
+      answer.push(`${i} ${v-i}`)
+      break;
+    }
+  }
+});
+console.log(answer.join('\n'));
