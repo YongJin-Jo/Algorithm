a440c0eb324de503657dfd3f3f18bdeceffbc5d3
YongJin-Jo <artinc5233@gmail.com>
2022-03-16 16:12:20 +0900

    백준 브루트 포스 문제풀이: 영화감독 숌

--- 백준/단계별 풀이/브루트 포스/1436.js
+++ 백준/단계별 풀이/브루트 포스/1436.js
@@ -1,35 +1,12 @@
-const fs = '2'
-const input = fs.toString()
-const N = +input
-solution()
+let input = 500;
+let theNumberAtTheEnd = 665;
 
-function solution(){
-  let number =667
-  let count = 1
-  let answer
-  while(true){
-    let str = number.toString().length
+while (input > 0) {
+  theNumberAtTheEnd++;
 
-    if(N === 1){
-      console.log(666);
-      break
-    }
-    
-    for(let i=0; i<str; i++){
-      let arr = str.split('').map(itme =>+itme)
-      if(arr[i] === 6){
-        if(arr[i+1] ===6 && arr[i+2] ===6){
-          console.log(arr);
-
-        }
-      }
-    }
-    
-    if(count === N){
-      console.log(answer);
-      break;
-    }
-    number++
-
+  if (theNumberAtTheEnd.toString().includes('666')) {
+    input--;
   }
 }
+
+console.log(theNumberAtTheEnd);
