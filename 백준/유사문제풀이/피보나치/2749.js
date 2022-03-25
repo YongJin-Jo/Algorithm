225636a0cccce78483402a17cc111e6574cc2288
artinc5233 <artinc5233@gmail.com>
2022-03-25 13:46:34 +0900

    백준 유사문제풀이: 피보나치 수 (실패)

--- 백준/유사문제풀이/피보나치/2749.js
+++ 백준/유사문제풀이/피보나치/2749.js
@@ -1,9 +1,13 @@
 //재귀로 구현할시 시간 복잡도는 O(2**n) 재귀로 풀면 시간 초가 이므로 반복문을 써서 해결
 // 범위가 Num이 90까지여서  BigInt로 출력해야 해결할 수 있음 
+const fs = '1000'
+const input = +fs.toString()
 
+let result = 0
+let division = 1000000n
 let times = (...matrices) =>
   matrices.reduce(
-    ([a,b,c], [d,e,f]) => [a*d + b*e, a*e + b*f, b*e + c*f]
+    ([ a,b,c], [d,e,f]) => [a*d + b*e, a*e + b*f, b*e + c*f]
   );
 
 let power = (matrix, n) => {
@@ -17,11 +21,12 @@ let power = (matrix, n) => {
 }
 
 let fibonacci = (n) =>{
-  let answer =0
-  let division =1000000n
   n < 2
-  ? answer = BigInt(n)
-  : answer = BigInt(power([1, 1, 0], n - 1)[0])
-  return (answer%division).toString()
+  ? result= n
+  : result = power(new BigInt64Array([1n, 1n, 0n]) , n - 1)[0];
 }
-console.log(fibonacci(1000))
+
+fibonacci(input)
+
+let answer = result%division
+console.log(answer.toString());
