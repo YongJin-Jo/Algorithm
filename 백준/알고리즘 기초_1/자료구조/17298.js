a17c54d9bb2231fc4a38cf7b366dd534ed108031
artinc5233 <artinc5233@gmail.com>
2022-03-29 12:56:57 +0900

    백준 알고리즘 기초_1 문제 풀이: 오큰수, 오등큰수(풀이중)

--- 백준/알고리즘 기초_1/자료구조/17298.js
+++ 백준/알고리즘 기초_1/자료구조/17298.js
@@ -1,14 +1,13 @@
-const fs ='4\n3 5 2 7'
+const fs ='4\n9 5 4 8'
 const [N,...input] = fs.toString().split('\n')
-const array = input.map(item => +item)
-const stack =[]
-const answer = []
-for(let i =0; i< N; i++){
-  if(stack !==0){
+const array = input[0].split(' ').map(item => +item)
+let result = new Array(array.length).fill(-1);
+let stack = [];
+array.map((x, i) => {
+  while (stack.length && array[stack[stack.length - 1]] < x) {
+    result[stack.pop()] = x;
   }
-  if(array[i] < array[i+1]){
-    answer.push(array[i+1])
-  }else{
-    stack.push(array[i])
-  }
-}
+  stack.push(i);
+});
+
+console.log(result.join(" "));
---
+++ 백준/알고리즘 기초_1/자료구조/17299.js
@@ -0,0 +1,27 @@
+const fs = '7\n1 1 2 3 4 2 1'
+const [N,...input] = fs.toString().split('\n')
+const array= input[0].split(' ').map(item => +item)
+const count = new Map()
+const result = Array(array.length).fill(-1)
+const stack=[]
+
+for(key of array){
+  if(count.has(key)){
+    let value = count.get(key)
+    value++
+    count.set(key,value)
+  }else{
+    count.set(key,1)
+  }
+}
+
+array.map((x,i)=>{
+  let number = array[stack[stack.length-1]]
+  console.log(number,count.get(number),x,count.get(x)); 
+  while(stack.length && count.get(number) < count.get(x)){
+    result[stack.pop()] =x
+  }
+  stack.push(i)
+})
+
+console.log(result);
