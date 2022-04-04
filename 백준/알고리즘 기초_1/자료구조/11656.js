7a626f8a15cf254d6381c8de527bf061f2d36037
YongJin-Jo <artinc5233@gmail.com>
2022-04-04 23:05:23 +0900

    백준 알고리즘 기초_1 문제 풀이: 접미사 배열

--- 백준/알고리즘 기초_1/자료구조/11656.js
+++ 백준/알고리즘 기초_1/자료구조/11656.js
@@ -1,7 +1,14 @@
 const fs = 'baekjoon'
+const S = fs.toString().split('')
+const len = S.length
 const array =[]
-for(let i=0; i<fs.length; i++){
-  if(i ===0) array.push(fs)
+for(let i=0; i<len; i++){
+  if(i ===0){
+    array.push(S.join(''))
+  }else{
+    S.shift()
+    array.push(S.join(''))
+  }
 }
 
-console.log(array);
+console.log(array.sort().join('\n'));
