e6d572c789ffc1a26ff2c8b4ee5545217a5900fd
artinc5233 <artinc5233@gmail.com>
2022-04-21 17:22:31 +0900

    이코테 다이나믹프로그래밍 문제 풀이: 금광

--- 이코테/다이나믹프로그래밍/금광.js
+++ 이코테/다이나믹프로그래밍/금광.js
@@ -1,18 +1,45 @@
 const fs ='2\n3 4\n1 3 3 2 2 1 4 1 0 6 4 7\n4 4\n1 3 1 5 2 2 4 1 5 0 2 3 0 6 1 2'
 const input = fs.toString().trim().split('\n')
 const T = input.shift()
-let obj ={}
-for(let i=0; i<T; i++){
-  let [n,m] =input.shift().split(' ').map(item => +item)
-  let l = input.shift().split(' ').map(item => +item)
-  let array = []
-  for(let i=0; i<n; i++){
-    const b = []
-    for(let j=0; j<m; j++){
-      b.push(l.shift())
+
+soution(T,input)
+
+function soution(T,input){
+  for(let i=0; i<T; i++){
+    let [n,m] =input.shift().split(' ').map(item => +item)
+    let l = input.shift().split(' ').map(item => +item)
+    let array = strSplit(n,m,l)
+    let dp = [...array]
+    let reulst =0
+    
+    for(let j=1; j<m; j++){
+      for(let k=0; k <n;k++){
+        let leftUp, leftDown,left
+        if(k==0) leftUp=0;
+        else leftUp = dp[k-1][j-1]
+        if(k == n-1) leftDown = 0;
+        else leftDown = dp[k+1][j-1]
+        left = dp[k][j-1];
+        dp[k][j] = dp[k][j]+ Math.max(leftUp,leftDown,left);
+      }
     }
-    array.push(b)
+    for(let i=0; i<n; i++){
+      reulst= Math.max(reulst,dp[i][m-1])
+    }
+    console.log(reulst);
   }
-  obj[i]=array
 }
-console.log(obj);
+
+function strSplit(n,m,l){
+  const result =[]
+    for(let i=0; i<n; i++){
+      const b = []
+      for(let j=0; j<m; j++){
+        b.push(l.shift())
+      }
+      result.push(b)
+    }
+  return result
+}
+
+
