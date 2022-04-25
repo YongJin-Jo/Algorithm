b9f55f254a0288d66bced744bf932a9a424a4f6c
YongJin-Jo <artinc5233@gmail.com>
2022-04-25 22:59:25 +0900

    백준 알고리즘 기초_1 다이나믹프로그래밍: 1,2,3 더하기, 카드 구매하기(풀이중)

---
+++ 백준/알고리즘 기초_1/다이나믹프로그래밍/11052.js
@@ -0,0 +1,3 @@
+const  fs = '4\n1 5 6 7'
+const [T,input] = fs.toString().trim().split('\n')
+const array = input.toString().split(' ').map(item => +item)
---
+++ 백준/알고리즘 기초_1/다이나믹프로그래밍/9095.js
@@ -0,0 +1,17 @@
+const fs ='3\n4\n7\n10'
+const [T,...input] = fs.toString().trim().split('\n')
+let dp =[0,1,2,4]
+
+for(let i=0; i<T; i++){
+  console.log(soultion(+input[i])); 
+}
+
+function soultion(target){
+  if(target === 1 || target === 2 || target === 3){
+    return dp[target]
+  }
+  for(let i=4; i<=target; i++){
+    dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+  }
+  return dp[target]
+}
