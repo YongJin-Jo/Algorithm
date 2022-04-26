ec47b270e01058e8f52e802054f25251c0ef8b00
YongJin-Jo <artinc5233@gmail.com>
2022-04-27 00:06:42 +0900

    백준 알고리즘 기초_1 다이나믹프로그래밍: 카드 구매하기(실패)

--- 백준/알고리즘 기초_1/다이나믹프로그래밍/11052.js
+++ 백준/알고리즘 기초_1/다이나믹프로그래밍/11052.js
@@ -1,3 +1,18 @@
-const  fs = '4\n1 5 6 7'
+const  fs = '4\n3 5 15 16'
 const [T,input] = fs.toString().trim().split('\n')
-const array = input.toString().split(' ').map(item => +item)
+const dp = input.toString().split(' ').map(item => +item)
+dp.unshift(0)
+
+for(let i=2; i<=T; i++){
+  if(i <=2){
+    dp[i] = Math.max(dp[i],dp[1]+dp[1])
+  }else{
+    let count = 1
+    while (count<Math.ceil(i/2)) {
+      let m = i-count
+      dp[i] = Math.max(dp[i],dp[m]+dp[count])
+      count++
+    }
+  }
+}
+console.log(dp[T]);
