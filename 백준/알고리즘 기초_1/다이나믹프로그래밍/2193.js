80f81f61edb20da143bdb8c3e0e6c33b4fc8bfda
YongJin-Jo <artinc5233@gmail.com>
2022-05-01 23:03:07 +0900

    백준 알고리즘 기초_1 다이나믹프로그래밍 : 이친수

--- 백준/알고리즘 기초_1/다이나믹프로그래밍/2193.js
+++ 백준/알고리즘 기초_1/다이나믹프로그래밍/2193.js
@@ -1,32 +1,12 @@
 //1. 0으로 시작하지 않는다.
 //2. 1이 두번 연속으로 나타나지 않는다.
-
-const fs = '0'
+const fs = '90'
 const N = +fs.toString().trim()
-const array = Array(90+1).fill(0)
-const dp = JSON.parse(JSON.stringify(array))
-dp[1] = dp[2] = 1
-dp[3] = 2 
+const dp = [[0,0],[0,1],[1,0]]
+let count =0
 
-for(let i=4; i<=N; i++){
-  let count = bineryCheck(i) 
-  dp[i] = dp[i-1] + count
+for(let i=3; i<=N; i++){
+  dp[i] = [BigInt(dp[i-1][0])+BigInt(dp[i-1][1]),BigInt(dp[i-1][0])]
 }
-console.log(dp[N]);
 
-function bineryCheck(number){
-  let binery = []  
-  while (1 < number) {
-    let M = parseInt(number%2) 
-    number = parseInt(number/2) 
-    binery.push(M)
-    if(number <=1){binery.push(parseInt(number))}
-  }
-
-  binery.reverse()
-  for(let i=0; i < binery.length; i++){
-    if(binery[0] === 0) return 0
-    if(binery[i] ===1 && binery[i+1] ===1  ) return 0
-  }
-  return 1
-}
+console.log(BigInt(dp[N][0]+dp[N][1]).toString());
