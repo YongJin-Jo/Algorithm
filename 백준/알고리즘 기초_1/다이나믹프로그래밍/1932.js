270bca0e7718d5e3e163144ce294cbcaa35b9f0c
YongJin-Jo <artinc5233@gmail.com>
2022-05-18 23:59:01 +0900

    백준 알고리즘_기초 1 다이나믹프로그래밍: 정수 삼각형(풀이중)

---
+++ 백준/알고리즘 기초_1/다이나믹프로그래밍/1932.js
@@ -0,0 +1,17 @@
+const fs ='5\n7\n3 8\n8 1 0\n2 7 4 4\n4 5 2 6 5'
+const [n,...ary] = fs.toString().trim().split('\n')
+let dt = ary.map(item => item.split(' ').map(_=> +_))
+let dp =new Array(+n).fill().map(_ => Array(+n).fill(0))
+dp[0][0] = dt[0][0]
+
+for(let i=1; i<n; i++){
+  for(let j=0; j<=i; j++){
+    if(j>0){
+
+      dp[i][j] = dp[i-1][j-1] +dt[i][j]
+      dp[i][j] = Math.max(dp[i][j],dp[i-1][j] + dt[i][j])
+    }
+  }
+}
+
+console.log(dp);
