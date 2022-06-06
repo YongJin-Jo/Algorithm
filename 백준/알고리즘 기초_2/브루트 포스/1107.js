13035c941888cc39f2a74fac9fe7687af9bd69a5
artinc5233 <artinc5233@gmail.com>
2022-06-06 17:38:31 +0900

    백준 알고리즘 기초_2 브루트 포스: 리모컨(실패)

--- 백준/알고리즘 기초_2/브루트 포스/1107.js
+++ 백준/알고리즘 기초_2/브루트 포스/1107.js
@@ -1,36 +1,44 @@
-const fs ='80000\n2\n8 9'
-const [N,M,arr] = fs.split('\n')
-const Narr = N.split('').map(_ => +_)
-const Barr = arr.split(' ').map(_=> +_)
-const answer = []
-let count =0
+const fs ='99999\n1\n9'
+const input = fs.toString().trim().split('\n')
+const currentWatingChnal = Math.abs((+input[0])-100)
 
+console.log(Math.min(currentWatingChnal,soultion(input)) );
+function soultion(input){
+  if(input.length <=2){return input[0].length}
+  const TargetNumber = input[0].split('').map(_=>+_)
+  const UnStableButton= input[2].split(' ').map(_ => +_)
+  const Button = [0,1,2,3,4,5,6,7,8,9]
+  const StableButton = Button.filter(item => !UnStableButton.includes(item))
+  const answerPush = [] 
+  let count
 
-for(let i=0; i<Narr.length; i++){
-  let number = Narr[i]
-  while (Barr.includes(number)) {
-      number--
-  }
-  answer.push(number)
-  count++
-}
-console.log(answer);
-let A = +N
-let B = +answer.join('')
-
-if(A<B){
-  while (A<B) {
-    A++
+  for(let i=0; i<TargetNumber.length; i++){
     count++
+    let but = TargetNumber[i]
+    let max =0
+    let maxButton =0
+    let min = 99
+    let minButton=0
+    for(let j=0; j<StableButton.length; j++){
 
+      if(answerPush.length <=0 || answerPush[0] >= TargetNumber[0]){
+        if(Math.abs(but-StableButton[j]) < min){
+          min = Math.abs(but-StableButton[j])
+          minButton = StableButton[j]
+        }
+      }else{
+        if(Math.abs(but-StableButton[j]) < min){
+          max = Math.abs(but-StableButton[j])
+          maxButton = StableButton[j]
+        }
+      }
+    }
+    answerPush.length <=0 || answerPush[0] >= TargetNumber[0]? answerPush.push(minButton):answerPush.push(maxButton)
   }
-}else{
-  while (B<A) {
-    B++
-    count++
-  }
+  count = (+answerPush.join('')).toString()
+  return  Math.abs((+input[0])-(+answerPush.join('')))+count.length
 }
 
 
 
-console.log(count );
+
