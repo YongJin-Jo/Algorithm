010bf6be53bd6ae5dc7a257eb54d9276c0294581
YongJin-Jo <artinc5233@gmail.com>
2022-06-07 02:12:00 +0900

    백준 알고리즘 기초_2 브루트 포스: 리모컨

--- 백준/알고리즘 기초_2/브루트 포스/1107.js
+++ 백준/알고리즘 기초_2/브루트 포스/1107.js
@@ -1,8 +1,8 @@
 const fs ='99999\n1\n9'
 const input = fs.toString().trim().split('\n')
 const currentWatingChnal = Math.abs((+input[0])-100)
-
-console.log(Math.min(currentWatingChnal,soultion(input)) );
+soultion(input)
+//console.log(Math.min(currentWatingChnal,) );
 function soultion(input){
   if(input.length <=2){return input[0].length}
   const TargetNumber = input[0].split('').map(_=>+_)
@@ -10,8 +10,9 @@ function soultion(input){
   const Button = [0,1,2,3,4,5,6,7,8,9]
   const StableButton = Button.filter(item => !UnStableButton.includes(item))
   const answerPush = [] 
+  const oneNine = []
   let count
-
+  if(TargetNumber[0] === 9){oneNine.push(Math.min(...StableButton.filter(item => item != 0)))}
   for(let i=0; i<TargetNumber.length; i++){
     count++
     let but = TargetNumber[i]
@@ -19,15 +20,40 @@ function soultion(input){
     let maxButton =0
     let min = 99
     let minButton=0
-    for(let j=0; j<StableButton.length; j++){
+    if(TargetNumber[0] === 9){
 
+      let max2 =-1
+      let maxButton2 = 0
+      for(let j=0; j<StableButton.length; j++){
+        if(Math.abs(but-StableButton[j]) >= max2){
+          max2 = Math.abs(but-StableButton[j])
+          maxButton2 = StableButton[j]
+        }
+      }
+      oneNine.push(maxButton2)
+      console.log(oneNine);
+    }
+    
+    if(i >0 && TargetNumber[0] === 1){
+      let min2 =99
+      let minButton2 = 0
+      for(let j=0; j<StableButton.length; j++){
+        if(Math.abs(but-StableButton[j]) < max2){
+          min2 = Math.abs(but-StableButton[j])
+          minButton2 = StableButton[j]
+        }
+      }
+      oneNine.push(minButton2)
+    }
+
+    for(let j=0; j<StableButton.length; j++){
       if(answerPush.length <=0 || answerPush[0] >= TargetNumber[0]){
         if(Math.abs(but-StableButton[j]) < min){
           min = Math.abs(but-StableButton[j])
           minButton = StableButton[j]
         }
       }else{
-        if(Math.abs(but-StableButton[j]) < min){
+        if(Math.abs(but-StableButton[j]) > max){
           max = Math.abs(but-StableButton[j])
           maxButton = StableButton[j]
         }
@@ -38,6 +64,7 @@ function soultion(input){
   count = (+answerPush.join('')).toString()
   return  Math.abs((+input[0])-(+answerPush.join('')))+count.length
 }
+
 
 
 
