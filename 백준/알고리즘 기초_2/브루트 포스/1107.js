93519dbddcc69fb7b95e6372cb148495121995ae
artinc5233 <artinc5233@gmail.com>
2022-06-07 16:50:55 +0900

    백준 알고리즘 기초_2 브루트 포스: 리모컨

--- 백준/알고리즘 기초_2/브루트 포스/1107.js
+++ 백준/알고리즘 기초_2/브루트 포스/1107.js
@@ -1,71 +1,59 @@
-const fs ='99999\n1\n9'
+const fs ='99999\n1\n0 9' 
 const input = fs.toString().trim().split('\n')
-const currentWatingChnal = Math.abs((+input[0])-100)
-soultion(input)
-//console.log(Math.min(currentWatingChnal,) );
-function soultion(input){
-  if(input.length <=2){return input[0].length}
-  const TargetNumber = input[0].split('').map(_=>+_)
-  const UnStableButton= input[2].split(' ').map(_ => +_)
-  const Button = [0,1,2,3,4,5,6,7,8,9]
-  const StableButton = Button.filter(item => !UnStableButton.includes(item))
-  const answerPush = [] 
-  const oneNine = []
-  let count
-  if(TargetNumber[0] === 9){oneNine.push(Math.min(...StableButton.filter(item => item != 0)))}
-  for(let i=0; i<TargetNumber.length; i++){
-    count++
-    let but = TargetNumber[i]
-    let max =0
-    let maxButton =0
-    let min = 99
-    let minButton=0
-    if(TargetNumber[0] === 9){
+// 답안 작성
+let answer
+let n = input[0].split('');
+if(input[2]){
+  let arr = input[2].split(' ');
+  answer = remotecon(n,arr);
+}else{
+  answer = remotecon(n,[]);
+}    
+console.log(answer)
 
-      let max2 =-1
-      let maxButton2 = 0
-      for(let j=0; j<StableButton.length; j++){
-        if(Math.abs(but-StableButton[j]) >= max2){
-          max2 = Math.abs(but-StableButton[j])
-          maxButton2 = StableButton[j]
-        }
-      }
-      oneNine.push(maxButton2)
-      console.log(oneNine);
+function remotecon(n, arr=[]){
+  let button = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let diff = [];
+  let diff_max = [];
+  let result = Math.abs(n.join('')*1-100);
+  //고장난 버튼 찾기
+  arr.length >0? button = button.filter(item => !arr.includes(item)):null
+  //최대,최소 범위 지정  
+  if(button.length>0){
+    for(let i = 0 ; i<n.length;i++){
+      let min_btn = button.reduce((acc,cur)=>{
+        if ((acc-n[i]*1) != Math.min((acc-n[i]*1),(cur*1-n[i]*1)))
+        acc=cur;
+        return acc
+      });
+      diff.push(min_btn)
+      let max_btn = button.reduce((acc,cur)=>{
+        if ((acc-n[i]*1) != Math.max((acc-n[i]*1),(cur*1-n[i]*1)))acc=cur;
+        return acc
+      });
+      diff_max.push(max_btn)
     }
-    
-    if(i >0 && TargetNumber[0] === 1){
-      let min2 =99
-      let minButton2 = 0
-      for(let j=0; j<StableButton.length; j++){
-        if(Math.abs(but-StableButton[j]) < max2){
-          min2 = Math.abs(but-StableButton[j])
-          minButton2 = StableButton[j]
-        }
-      }
-      oneNine.push(minButton2)
-    }
-
-    for(let j=0; j<StableButton.length; j++){
-      if(answerPush.length <=0 || answerPush[0] >= TargetNumber[0]){
-        if(Math.abs(but-StableButton[j]) < min){
-          min = Math.abs(but-StableButton[j])
-          minButton = StableButton[j]
-        }
-      }else{
-        if(Math.abs(but-StableButton[j]) > max){
-          max = Math.abs(but-StableButton[j])
-          maxButton = StableButton[j]
-        }
-      }
-    }
-    answerPush.length <=0 || answerPush[0] >= TargetNumber[0]? answerPush.push(minButton):answerPush.push(maxButton)
   }
-  count = (+answerPush.join('')).toString()
-  return  Math.abs((+input[0])-(+answerPush.join('')))+count.length
+
+  // 한자리수 내림 값과 한자리수 올림값
+  if(diff.length>1)diff.pop();
+  diff_max[0] = 1+diff_max[0];
+  
+  //범위 내의 모든 경우에서 최소값 찾기
+  let min = 500000;
+  for(let i=diff.join('')*1;i<=(diff_max.join(''))*1;i++){
+    if(compare_button(arr,i)){
+      min = Math.min(Math.abs(i*1-n.join('')*1)+ i.toString().length, min);    
+    }
+  }
+
+  return Math.min(result, min);
 }
-
-
-
-
-
+//고장난 버튼이 있는지 비교
+function compare_button(arr, i){  
+  i = i.toString().split('');  
+  for(let j = 0;j<i.length;j++){
+    if(arr.includes(i[j]))return false;
+  }  
+  return true;
+}
