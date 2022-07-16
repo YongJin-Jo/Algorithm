be1f440338f5d8616bb3bb0fd8f7ed269f3a1588
YongJin-Jo <artinc5233@gmail.com>
2022-07-16 23:15:16 +0900

    백준 알고리즘 기초_2 순열: 이전순열(실패)

---
+++ 백준/알고리즘 기초_2/브루트 포스/순열/10973.js
@@ -0,0 +1,24 @@
+const fs= '4\n4 2 1 3'
+const [ N, arr ] = fs.toString().trim().split('\n')
+const dt = arr.split(' ').map(_ => +_)
+const solve = (N, arr) => {
+  const arrCopy = [...arr].sort((a, b) => a-b);
+  if (arr.every((v, i) => v === arrCopy[i])) {
+    console.log(-1);
+  } else {
+    let i = N - 2;
+    while (arr[i] < arr[i+1]) {
+      i--;
+    }
+
+    let j = N - 1;
+    while (arr[i] < arr[j]) {
+      j--;
+    }
+    
+    [ arr[i], arr[j] ] = [ arr[j], arr[i] ];
+    console.log([...arr.slice(0, i+1), ...arr.slice(i+1).sort((a, b) =>a-b)].join(' '));
+  }
+};
+
+solve(+N, dt);
--- 알고리즘 모음/탐색/premutaion_순열.js
+++ 알고리즘 모음/탐색/premutaion_순열.js
@@ -1,13 +1,38 @@
-const nextPermutation = function(nums) {
-  const swap = (arr, a, b) => [ arr[a], arr[b] ] = [ arr[b], arr[a] ]
-  
-  let i = nums.length -2
-  while(0 <= i && nums[i] >= nums[i+1]) i--
-  
-  if (0 <= i) {
-      let j = nums.length-1
-      while(nums[i] >= nums[j]) j--
-      swap(nums, i, j)
+// 다음 순열
+const nextPermutation = function(N, arr){
+  const arrCopy = [...arr].sort((a, b) => b - a);
+  if (arr.every((v, i) => v === arrCopy[i])) {
+    console.log(-1);
+  } else {
+    let i = N - 2;
+    while (arr[i] > arr[i+1]) {
+      i--;
+    }
+
+    let j = N - 1;
+    while (arr[i] > arr[j]) {
+      j--;
+    }
+    
+    [ arr[i], arr[j] ] = [ arr[j], arr[i] ];
+    console.log([...arr.slice(0, i+1), ...arr.slice(i+1).sort((a, b) => a - b)].join(' '));
   }
-  nums.push(...nums.splice(i+1).reverse())
 };
+
+
+// 이전 순열
+const prevPermutation = function(N, arr){
+  const arrCopy = [...arr].sort((a,b) => a-b)
+  if(arr.every((v,i) => v === arrCopy[i])){
+    console.log(-1);
+    return
+  }
+
+  let i=N-2;
+  let j=N-1;
+  while(arr[i] > arr[i+1])  i--;
+  while(arr[i] > arr[j]) j--;
+
+  [arr[i],arr[j]] = [arr[j],arr[i]];
+  console.log([...arr.slice(0, i+1), ...arr.slice(i+1).sort((a, b) => b-a)].join(' '));
+}
